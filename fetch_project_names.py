f54a8f1dbe23251c98f59201c9996b1dc9c76487
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2021-12-27 15:07:05 +0100

    Project fetching system COMPLETE!!!

--- fetch_project_names.py
+++ fetch_project_names.py
@@ -2,8 +2,8 @@
 import os
 
 # html
-f = open("html.txt", "w")
-root = "./web"
+f = open("links/html.txt", "w")
+root = "./links/web"
 for name in os.listdir(root):
     if not os.path.isfile(os.path.join(root, name)):
         f.write(name + "\n")
@@ -12,8 +12,8 @@ f.close()
 # other
 others = ["py", "java"]
 for other in others:
-    f = open(f"{other}.txt", "w")
-    for _, _, files in os.walk(f"file\{other}"):
+    f = open(f"links/{other}.txt", "w")
+    for _, _, files in os.walk(f"links\\file\{other}"):
         for name in files:
             try:
                 f.write(name + "\n")
