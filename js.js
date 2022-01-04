6befbb03c636833d789f0db04781048db6ef59a3
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-01-04 19:06:58 +0100

    final?

--- js.js
+++ js.js
@@ -15,27 +15,47 @@ function setTheme(){
     if(theme=="light")
     {
         //replace light images with dark
+        //empty
         $("header>.theme>.theme_button").empty();
         $(".link_type_github").empty();
         $(".link_type_link").empty();
         $(".link_type_download").empty();
+        $(".pre_portfolio").empty();
+        $(".pre_projects").empty();
+        $(".pre_html_playground").empty();
+        //theme
         $("header>.theme>.theme_button").append(`<img src="img/moon.png" alt="Dark mode">`);
+        //galery
         $(".link_type_github").append(`<img src="img/git.png" alt="GitHub">`);
         $(".link_type_link").append(`<img src="img/link.png" alt="Link">`);
         $(".link_type_download").append(`<img src="img/download.png" alt="Download">`);
+        //preview
+        $(".pre_portfolio").append(`<img src="img/preview/portfolio.png" alt="Portfolio">`);
+        $(".pre_projects").append(`<img src="img/preview/projects.png" alt="Projektek">`);
+        $(".pre_html_playground").append(`<img src="img/preview/html_playground.png" alt="HTML készítő">`);
         theme = "dark";
     }
     else
     {
         //replace dark images with light
+        //empty
         $("header>.theme>.theme_button").empty();
         $(".link_type_github").empty();
         $(".link_type_link").empty();
         $(".link_type_download").empty();
+        $(".pre_portfolio").empty();
+        $(".pre_projects").empty();
+        $(".pre_html_playground").empty();
+        //theme
         $("header>.theme>.theme_button").append(`<img src="img/sun.png" alt="Light mode">`);
+        //galery
         $(".link_type_github").append(`<img src="img/git_dark.png" alt="GitHub">`);
         $(".link_type_link").append(`<img src="img/link_dark.png" alt="Link">`);
         $(".link_type_download").append(`<img src="img/download_dark.png" alt="Download">`);
+        //preview
+        $(".pre_portfolio").append(`<img src="img/preview/portfolio_dark.png" alt="Portfolio">`);
+        $(".pre_projects").append(`<img src="img/preview/projects_dark.png" alt="Projektek">`);
+        $(".pre_html_playground").append(`<img src="img/preview/html_playground_dark.png" alt="HTML készítő">`);
         theme = "light";
     }
     document.documentElement.className = theme;
